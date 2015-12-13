7b542438ccaffa5f2fbb48b4ee5b67d850f04b64
enosent <enosent7@gmail.com>
2015-12-13 19:26:03 +0900

    테스트 코드 수정

--- src/test/java/camp/test/gson/GsonTest.java
+++ src/test/java/camp/test/gson/GsonTest.java
@@ -19,34 +19,38 @@ public class GsonTest {
 		final CountDownLatch startLatch = new CountDownLatch(1);
 		final CountDownLatch finishLatch = new CountDownLatch(10);
 		final AtomicBoolean failed = new AtomicBoolean(false);
-		
+
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 
-		for (int taskCount = 0; taskCount < 10; taskCount++) {
-			executor.execute(new Runnable() {
-				public void run() {
-					System.out.println("# " + Thread.currentThread().getName() );
-					
-					Hello myObj = new Hello("world");
-					try {
-						startLatch.await(); // 하나의 Task가 끝날때까지 대기
-						
-						for (int i = 0; i < 10; i++) {
-							gson.toJson(myObj);
+		for(int requst = 0; requst < 4000; requst++ ) {
+
+			for (int taskCount = 0; taskCount < 10; taskCount++) {
+				executor.execute(new Runnable() {
+					public void run() {
+						// System.out.println("# " + Thread.currentThread().getName() );
+
+						Hello myObj = new Hello("world");
+						try {
+							startLatch.await(); // 하나의 Task가 끝날때까지 대기
+
+							for (int i = 0; i < 10; i++) {
+								gson.toJson(myObj);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							failed.set(true); // 실패
+						} finally {
+							finishLatch.countDown(); // 처리완료
 						}
-					} catch (Exception e) {
-						e.printStackTrace();
-						failed.set(true); // 실패
-					} finally {
-						finishLatch.countDown(); // 처리완료
 					}
-				}
-			});
+				});
+			}
+
 		}
-		
+
 		startLatch.countDown(); // Task 완료
 		finishLatch.await(); // 다른 Thread 처리까지 대기
-		
+
 		assertFalse(failed.get());
 	}
 
