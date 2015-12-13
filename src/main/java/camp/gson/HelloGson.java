9b773bc23c117d3e66e3cb5f0d6f846e625bcee3
enosent <enosent7@gmail.com>
2015-12-13 19:12:59 +0900

    Initial commit

---
+++ src/main/java/camp/gson/HelloGson.java
@@ -0,0 +1,10 @@
+package camp.gson;
+
+public class HelloGson {
+
+	public static void main(String[] arg) {
+
+		System.out.println("Hello Gson!");
+
+	}
+}
---
+++ src/test/java/camp/test/gson/DefaultTest.java
@@ -0,0 +1,16 @@
+package camp.test.gson;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DefaultTest {
+	
+	@Test
+	public void defaultTest() {
+		String msg = "Hello Gson";
+		assertNotNull(msg);
+	}
+
+}
